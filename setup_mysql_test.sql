b4a9ccac5b5707569d5a3f39d6b8bbc085e430b6
AudreyLican <audrey.lican@gmail.com>
2024-03-25 22:03:16 +0100

    create task 4 files

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+#!/bin/bash
+-- script that prepares a MySQL server for the project
+-- create DB 'hbnb_test_db' and user 'hbnb_test'
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

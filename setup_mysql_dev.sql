c447a1f97777a023130b9e1c3e46ad145c8d3425
AudreyLican <audrey.lican@gmail.com>
2024-03-25 22:00:27 +0100

    added semi-colon to each statement

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,15 +2,15 @@
 -- script that prepares a MySQL server for the project
 
 -- create DB named 'hbnb_dev_db' and user 'hbnb_dev_db'
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db
-USE hbnb_dev_db
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- create DB 'hbnb_test_db' and user 'hbnb_test'
-CREATE DATABASE IF NOT EXISTS hbnb_test_db
-USE hbnb_test_db
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

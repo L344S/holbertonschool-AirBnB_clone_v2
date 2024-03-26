775015e447db44d82f41796fe8abe104afc8c04b
L344S <145486211+L344S@users.noreply.github.com>
2024-03-26 18:56:03 +0100

    Fix: PYCS NOW OK

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -67,6 +67,6 @@ class DBStorage:
         """Create all tables and the current database session"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
-                                      expire_on_commit=False)
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()

30ad5e792f2a82faaefe762abf18ac50d56c2b4e
L344S <145486211+L344S@users.noreply.github.com>
2024-03-29 11:04:45 +0100

    Updated setup_mysql_test.sql file in the repository
    PYCS OK | LOC OK | GDS OK
    
    Commit done automatically

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,6 @@
-#!/bin/bash
--- script that prepares a MySQL server for the project
--- create DB 'hbnb_test_db' and user 'hbnb_test'
+-- Active: 1710694390878@@127.0.0.1@3306@hbnb_test_db
+-- TASK: Create a database and user for the test environment of Rbnb project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

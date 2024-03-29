0b846d91c01805809f586d01ea6fcef49421f4ff
L344S <145486211+L344S@users.noreply.github.com>
2024-03-29 11:04:45 +0100

    Updated setup_mysql_dev.sql file in the repository
    PYCS OK | LOC OK | GDS OK
    
    Commit done automatically

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,6 @@
--- SQLBook: Code
-#!/bin/bash
--- script that prepares a MySQL server for the project
--- create DB named 'hbnb_dev_db' and user 'hbnb_dev_db'
+-- Active: 1710694390878@@127.0.0.1@3306@hbnb_dev_db
+-- TASK: Create a database and user for the development environment of Rbnb project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;

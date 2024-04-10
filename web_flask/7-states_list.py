1548acc6a2104fdc0ee9d800fa778a3f3955bca8
L344S <7474@holbertonstudents.com>
2024-04-10 23:56:13 +0200

    Added 7-states_list.py to the repository
    LOCAL OK | PYCS OK | GDS OK

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """Method to remove the current session"""
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Method to display a HTML page with states"""
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Method to remove the current session"""
+    storage.close()
 
 
 if __name__ == "__main__":

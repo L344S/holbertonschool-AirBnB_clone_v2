4a9bc8fcd8c704397170613eb08511d97cca558d
L344S <145486211+L344S@users.noreply.github.com>
2024-04-10 20:36:40 +0200

    Updated db_storage.py in the repository

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -61,3 +61,7 @@ class DBStorage:
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Method to close the current session"""
+        self.__session.close()

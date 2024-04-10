cdf4fec2cdbd222229b713b80a0998cd1a9944dc
L344S <145486211+L344S@users.noreply.github.com>
2024-04-10 20:58:26 +0200

    Updated 7-states_list.py in the repository

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,17 +4,19 @@ from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
+
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Closes session"""
+    """Method to remove the current session"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
-def showstates():
+def states_list():
     """Method to display a HTML page with states"""
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
@@ -22,4 +24,4 @@ def showstates():
 
 if __name__ == "__main__":
     # App must be listening on 0.0.0.0, port 5000
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
